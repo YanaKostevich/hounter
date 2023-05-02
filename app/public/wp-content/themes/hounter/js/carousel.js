1c69bbadd7e5b241248125814cc400c39c030050
Jane Kostevich <yana.kostevich18@gmail.com>
2023-05-02 04:21:08 +0300

    slider Featured House

--- app/public/wp-content/themes/hounter/js/carousel.js
+++ app/public/wp-content/themes/hounter/js/carousel.js
@@ -1,6 +1,21 @@
+let items1 = document.querySelectorAll(".carousel-1 .carousel-item");
 let items = document.querySelectorAll(".carousel .carousel-item");
 let nextButton = document.querySelector(".carousel-control-next");
 let prevButton = document.querySelector(".carousel-control-prev");
+
+items1.forEach((el) => {
+  const minPerSlide = 3;
+  let next = el.nextElementSibling;
+  for (var i = 1; i < minPerSlide; i++) {
+    if (!next) {
+      // wrap carousel by using first child
+      next = items1[0];
+    }
+    let cloneChild = next.cloneNode(true);
+    el.appendChild(cloneChild.children[0]);
+    next = next.nextElementSibling;
+  }
+});
 
 items.forEach((el) => {
   const minPerSlide = 4;
--- app/public/wp-content/themes/hounter/page-homepage.php
+++ app/public/wp-content/themes/hounter/page-homepage.php
@@ -39,7 +39,7 @@ get_header();
 			<div class="slider-right-content">
 				<div class="text-center my-3">
 					<div class="row mx-auto my-auto justify-content-center">
-						<div class="carousel slide" data-bs-ride="carousel" data-bs-interval="3000"> <!-- carousel -->
+						<div class="carousel-1 slide" data-bs-ride="carousel" data-bs-interval="3000"> <!-- carousel -->
 							<div class="carousel-inner" role="listbox">
 								<div class="carousel-item active">
 									<div class="col-md-3">
@@ -184,92 +184,252 @@ get_header();
 				</button>
 			</div>
 		</div>
+
+
 		<div class="tab-content" id="myTabContent">
 			<div class="tab-pane fade show active" id="house-tab-pane" role="tabpanel" aria-labelledby="house-tab" tabindex="0">
-				<div class="text-center my-3">
+				<div class="text-center my-4">
 					<div class="row mx-auto my-auto justify-content-center">
-						<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel"> <!-- carousel -->
-							<div class="carousel-inner" role="listbox">
+						<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+							<div class="carousel-inner">
 								<div class="carousel-item active">
-									<div class="col-md-3">
-										<div class="card">
-											<div class="card-img">
-												<?php
-												$image = get_field('people_image_for_info_slider');
-												if (!empty($image)) : ?>
-													<img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
-												<?php endif; ?>
+									<div class="col-md-4">
+										<div class="body">
+											<div class="body-img">
+												<div class="img">
+													<?php
+													$image = get_field('image_house');
+													if (!empty($image)) : ?>
+														<img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+													<?php endif; ?>
+												</div>
+												<div class="text_absolute">
+													<div class="icons">
+														<?php
+														$image = get_field('icons_for_house');
+														if (!empty($image)) : ?>
+															<img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+														<?php endif; ?>
+													</div>
+													<div class="text_houses"><?php the_field('title_icons'); ?></div>
+												</div>
+
 											</div>
 											<div class="card-img">
-												<h5>
-													<?php the_field('people_title_for_info_slider'); ?>
-												</h5>
-												<h6>
-													<?php the_field('people_subtitle_for_info_slider'); ?>
-												</h6>
-											</div>
-										</div>
-									</div>
-								</div>
-								<div class="carousel-item">
-									<div class="col-md-3">
-										<div class="card">
-											<div class="card-img">
-												<?php
-												$image = get_field('house_image_for_info_slider');
-												if (!empty($image)) : ?>
-													<img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
-												<?php endif; ?>
-											</div>
-											<div class="card-img">
-												<h5>
-													<?php the_field('house_title_for_info_slider'); ?>
-												</h5>
-												<h6>
-													<?php the_field('house_subtitle_for_info_slider'); ?>
-												</h6>
-											</div>
-										</div>
-									</div>
-								</div>
-								<div class="carousel-item">
-									<div class="col-md-3">
-										<div class="card">
-											<div class="card-img">
-												<?php
-												$image = get_field('clients_image_for_info_slider');
-												if (!empty($image)) : ?>
-													<img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
-												<?php endif; ?>
-											</div>
-											<div class="card-img">
-												<h5>
-													<?php the_field('clients_title_for_info_slider'); ?>
-												</h5>
-												<h6>
-													<?php the_field('clients_subtitle_for_info_slider'); ?>
-												</h6>
+												<div class="title_price">
+													<p class="title"><?php echo esc_html(get_field('title_roselands_house')); ?></p>
+													<p class="price">$<?php echo esc_html(get_field('price_roselands_house')); ?></p>
+												</div>
+												<div class="person">
+													<div class="avatar">
+														<?php
+														$image = get_field('avatar');
+														if (!empty($image)) : ?>
+															<img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+														<?php endif; ?>
+													</div>
+													<div class="name_city">
+														<div class="name">
+															<?php the_field('name'); ?>
+														</div>
+														<div class="city">
+															<?php the_field('city'); ?>
+														</div>
+													</div>
+												</div>
 											</div>
 										</div>
 									</div>
 								</div>
 								<div class="carousel-item">
-									<div class="col-md-3">
-										<div class="card">
-											<div class="card-img">
-												<?php
-												$image = get_field('house_image_for_info_slider');
-												if (!empty($image)) : ?>
-													<img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
-												<?php endif; ?>
+									<div class="col-md-4">
+										<div class="body">
+											<div class="body-img">
+												<div class="img">
+													<?php
+													$image = get_field('image_woodlandside');
+													if (!empty($image)) : ?>
+														<img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+													<?php endif; ?>
+												</div>
+												<div class="text_absolute">
+													<div class="icons">
+														<?php
+														$image = get_field('icons_for_woodlandside');
+														if (!empty($image)) : ?>
+															<img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+														<?php endif; ?>
+													</div>
+													<div class="text_wood"><?php the_field('title_icons_wood'); ?></div>
+												</div>
+
 											</div>
 											<div class="card-img">
-												<h5>
-													<?php the_field('house_title_for_info_slider'); ?>
-												</h5>
-												<h6>
-													<?php the_field('house_subtitle_for_info_slider'); ?>
-												</h6>
+												<div class="title_price">
+													<p class="title"><?php echo esc_html(get_field('title_woodlandside')); ?></p>
+													<p class="price">$<?php echo esc_html(get_field('price_woodlandside')); ?></p>
+												</div>
+												<div class="person">
+													<div class="avatar">
+														<?php
+														$image = get_field('avatar_wood');
+														if (!empty($image)) : ?>
+															<img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+														<?php endif; ?>
+													</div>
+													<div class="name_city">
+														<div class="name">
+															<?php the_field('name_wood'); ?>
+														</div>
+														<div class="city">
+															<?php the_field('city_wood'); ?>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="carousel-item">
+									<div class="col-md-4">
+										<div class="body">
+											<div class="body-img">
+												<div class="img">
+													<?php
+													$image = get_field('image_the_old_lighthouse');
+													if (!empty($image)) : ?>
+														<img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+													<?php endif; ?>
+												</div>
+												<div class="text_absolute">
+													<div class="icons">
+														<?php
+														$image = get_field('icons_for_the_old_lighthouse');
+														if (!empty($image)) : ?>
+															<img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+														<?php endif; ?>
+													</div>
+													<div class="text_light"><?php the_field('title_icons_light'); ?></div>
+												</div>
+											</div>
+											<div class="card-img">
+												<div class="title_price">
+													<p class="title"><?php echo esc_html(get_field('title_the_old_lighthouse')); ?></p>
+													<p class="price">$<?php echo esc_html(get_field('price_the_old_lighthouse')); ?></p>
+												</div>
+												<div class="person">
+													<div class="avatar">
+														<?php
+														$image = get_field('avatar_light');
+														if (!empty($image)) : ?>
+															<img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+														<?php endif; ?>
+													</div>
+													<div class="name_city">
+														<div class="name_light">
+															<?php the_field('name_light'); ?>
+														</div>
+														<div class="city">
+															<?php the_field('city'); ?>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="carousel-item">
+									<div class="col-md-4">
+										<div class="body">
+											<div class="body-img">
+												<div class="img">
+													<?php
+													$image = get_field('image_cosmos_house');
+													if (!empty($image)) : ?>
+														<img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+													<?php endif; ?>
+												</div>
+												<div class="text_absolute">
+													<div class="icons">
+														<?php
+														$image = get_field('icons_cosmos_house');
+														if (!empty($image)) : ?>
+															<img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+														<?php endif; ?>
+													</div>
+													<div class="text_cosmos"><?php the_field('title_icons_cosmos'); ?></div>
+												</div>
+											</div>
+											<div class="card-img">
+												<div class="title_price">
+													<p class="title"><?php echo esc_html(get_field('title_cosmos _house')); ?></p>
+													<p class="price">$<?php echo esc_html(get_field('price_cosmos _house')); ?></p>
+												</div>
+												<div class="person">
+													<div class="avatar_cosmos">
+														<?php
+														$image = get_field('avatar');
+														if (!empty($image)) : ?>
+															<img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+														<?php endif; ?>
+													</div>
+													<div class="name_city">
+														<div class="name">
+															<?php the_field('name_cosmos'); ?>
+														</div>
+														<div class="city">
+															<?php the_field('city_cosmos'); ?>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="carousel-item">
+									<div class="col-md-4">
+										<div class="body">
+											<div class="body-img">
+												<div class="img">
+													<?php
+													$image = get_field('image_the_old_lighthouse');
+													if (!empty($image)) : ?>
+														<img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+													<?php endif; ?>
+												</div>
+												<div class="text_absolute">
+													<div class="icons">
+														<?php
+														$image = get_field('icons_for_the_old_lighthouse');
+														if (!empty($image)) : ?>
+															<img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+														<?php endif; ?>
+													</div>
+													<div class="text_light"><?php the_field('title_icons_light'); ?></div>
+												</div>
+											</div>
+											<div class="card-img">
+												<div class="title_price">
+													<p class="title"><?php echo esc_html(get_field('title_the_old_lighthouse')); ?></p>
+													<p class="price">$<?php echo esc_html(get_field('price_the_old_lighthouse')); ?></p>
+												</div>
+												<div class="person">
+													<div class="avatar">
+														<?php
+														$image = get_field('avatar_light');
+														if (!empty($image)) : ?>
+															<img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+														<?php endif; ?>
+													</div>
+													<div class="name_city">
+														<div class="name">
+															<?php the_field('name_light'); ?>
+														</div>
+														<div class="city">
+															<?php the_field('city_light'); ?>
+														</div>
+													</div>
+												</div>
 											</div>
 										</div>
 									</div>
@@ -278,11 +438,13 @@ get_header();
 						</div>
 					</div>
 				</div>
+
 			</div>
 			<div class="tab-pane fade" id="villa-tab-pane" role="tabpanel" aria-labelledby="villa-tab" tabindex="0">ФФФФ</div>
 			<div class="tab-pane fade" id="apartment-tab-pane" role="tabpanel" aria-labelledby="apartment-tab" tabindex="0">ШШШШ</div>
 		</div>
 	</div>
+
 
 </main><!-- #main -->
 
